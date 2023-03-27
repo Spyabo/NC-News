241410e127908d805dd11a9b01bedfdca55ee05d
Spyabo <reesewright02@protonmail.com>
2023-03-27 15:05:49 +0100

    Remove useless req.params

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,7 +1,7 @@
 const fetchTopics = require("../models/fetchTopics.model.js");
 
 function getTopics(req, resp, next) {
-  fetchTopics(req.query)
+  fetchTopics()
     .then((topics) => {
       resp.status(200).send({ topics });
     })
