9a46196b0e394873e1e2f1d01252abda6eebe2cf
Spyabo <reesewright02@protonmail.com>
2023-03-27 14:24:18 +0100

    Remove errorHandlers

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
 const express = require("express");
 const getTopics = require("./controllers/topics.controller");
 
-const {
-  handlePSQL400s,
-  handleCustomErrors,
-  handle500Statuses,
-} = require("./controllers/errorHandlers.controllers");
-
 const app = express();
 
 app.get("/api/topics", getTopics);
-
-app.use(handlePSQL400s);
-app.use(handleCustomErrors);
-app.use(handle500Statuses);
 
 module.exports = app;
