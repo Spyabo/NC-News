cf488889b7b72c7d0d1d58f8b3b2c8f3bab53590
Spyabo <reesewright02@protonmail.com>
2023-03-27 12:09:28 +0100

    Setup app.js and errorHandlers

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const getTopics = require("./controllers/topics.controller");
+
+const {
+  handlePSQL400s,
+  handleCustomErrors,
+  handle500Statuses,
+} = require("./controllers/errorHandlers.controllers");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/api/topics", getTopics);
+
+app.use(handlePSQL400s);
+app.use(handleCustomErrors);
+app.use(handle500Statuses);
+
+module.exports = app;
