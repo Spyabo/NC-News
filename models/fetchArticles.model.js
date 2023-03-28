6429f21fe618aaa7f76409d9b659c814d1c0d2b5
Spyabo <reesewright02@protonmail.com>
2023-03-28 13:47:20 +0100

    model and controller for articles/_id/comments

--- models/fetchArticles.model.js
+++ models/fetchArticles.model.js
@@ -1,3 +1,4 @@
+const { ClientBase } = require("pg");
 const db = require("../db/connection.js");
 
 exports.fetchArticleFromID = (article_id) => {
@@ -15,21 +16,13 @@ exports.fetchArticleFromID = (article_id) => {
 };
 
 exports.fetchArticles = () => {
-  const comment_count = `(SELECT COUNT(*) FROM comments WHERE comments.article_id = articles.article_id)`;
   return db
     .query(
-      `SELECT 
-      article_id,
-      author,
-      title,
-      body,
-      topic,
-      created_at,
-      votes,
-      article_img_url,
-      CAST(${comment_count}AS INT) AS comment_count
-  FROM 
-      articles ORDER BY created_at DESC`
+      `SELECT articles.article_id, articles.author, articles.title, articles.body, articles.topic, articles.created_at, articles.votes, articles.article_img_url, CAST(COUNT(comments.article_id) AS INT) AS comment_count
+      FROM articles
+      LEFT JOIN comments ON articles.article_id = comments.article_id
+      GROUP BY articles.article_id
+      ORDER BY articles.created_at DESC`
     )
     .then((data) => {
       if (data.rows.length === 0) {
@@ -41,3 +34,29 @@ exports.fetchArticles = () => {
       return data.rows;
     });
 };
+
+exports.fetchArticleComments = (article_id) => {
+  return db
+    .query(
+      `SELECT * FROM comments WHERE comments.article_id = $1 ORDER BY created_at DESC`,
+      [article_id]
+    )
+    .then((data) => {
+      return data.rows;
+    });
+};
+
+async function checkArticleExists(article_id) {
+  return db
+    .query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
+    .then((data) => {
+      if (data.rowCount === 0) {
+        return Promise.reject({
+          status: 404,
+          msg: "Article not found",
+        });
+      }
+      return 0; //Thought returning a falsy value would be better
+    });
+}
+module.exports.checkArticleExists = checkArticleExists;
