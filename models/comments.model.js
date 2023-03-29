4c1ca04c94cd1784a5c82534f92538c4361c1f50
Spyabo <reesewright02@protonmail.com>
2023-03-29 11:39:26 +0100

    add controller and model for comments

--- models/comments.model.js
+++ models/comments.model.js
@@ -1,5 +1,20 @@
 const db = require("../db/connection.js");
+const { checkArticleExists } = require("../models/fetchArticles.model.js");
 
-exports.insertComment = (comment) => {
-  const { username, body } = comment;
+exports.insertComment = async (comment) => {
+  const { username, body, article_id } = comment;
+  if (await checkArticleExists(article_id)) {
+    return Promise.reject({
+      status: 404,
+      msg: "Articles not found",
+    });
+  }
+  return db
+    .query(
+      `INSERT INTO comments (body, author, article_id) VALUES ($1, $2, $3) RETURNING *`,
+      [body, username, article_id]
+    )
+    .then((result) => {
+      return result.rows[0];
+    });
 };
